45e73e21bd05b7299762fbdfc84ee6969c6a559a
Sudeep Maity <sudeep.nitg@gmail.com>
2019-05-04 11:58:55 -0500

    Added connector for cloud Mongo
    Find the credential in yml config file for cloud Db.

---
+++ Mongo-Connector/connector.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+import yaml
+
+with open("configDb.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+admin = cfg['mysql']['user']
+pwd = cfg['mysql']['password']
+host = cfg['mysql']['host']
+DB = cfg['mysql']['db']
+
+
+
+
+mongoCred = "mongodb+srv://" + admin + ":" + pwd + "@"+ host + "?retryWrites=true"
+
+
+client = MongoClient(mongoCred)
+# list database
+print(client.list_database_names())
+db = client.DB
+
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
+
+
