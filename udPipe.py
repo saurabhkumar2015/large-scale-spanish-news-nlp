23a932494d1c7dadcaf4e1061b2632bff022421f
Sudeep Maity <sudeep.nitg@gmail.com>
2019-05-06 20:15:50 -0500

    Added udPipe model and script for generating dependency parse

---
+++ udPipe.py
@@ -0,0 +1,19 @@
+from ufal.udpipe import Model, Pipeline, ProcessingError
+import json
+
+model = Model.load('./spanish-ancora-ud-2.3-181115.udpipe')
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+# print(data[0]['text'])
+
+
+pipeline = Pipeline(model, 'tokenize', Pipeline.DEFAULT, Pipeline.DEFAULT,"conllu")
+
+error = ProcessingError()
+
+for i in range(0, len(data)):
+    articleText = data[i]['text']
+    parsedArticle = pipeline.process(articleText, error)
+    print(parsedArticle)
