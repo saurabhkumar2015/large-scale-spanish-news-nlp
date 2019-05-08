9543fc7a0ed4659ee117aa6be9b0c26b7a2440f4
08vishal <vishal205014@gmail.com>
2019-05-08 15:32:33 -0500

    Add files via upload

---
+++ Crawler/web_scrapper1.py
@@ -0,0 +1,22 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+my_url='https://www.yucatan.com.mx/'
+uClient=uReq(my_url)
+page_html=uClient.read()
+uClient.close()
+page_soup=soup(page_html,"html.parser")
+containers=page_soup.findAll("h2",{"class":"g1-gamma g1-gamma-1st entry-title"})
+filename="url41.txt"
+f=open(filename,"w")
+#containersx=page_soup.findAll("div",{"class":"img-container"})
+#print(len(containersx))
+#for container in containersx:
+##   print("https://www.milenio.com"+container.a["href"])
+  #  f.write("https://www.milenio.com"+container.a["href"]+"\n")
+
+for container in containers:
+
+    print(container.a["href"])
+    f.write(container.a["href"]+"\n")
+
